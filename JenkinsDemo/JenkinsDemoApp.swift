6f5656622fb8cfaf8c6fac570a61e33be0fcf247
Philip Al-Twal <philipal-twal@PhilipsrcuryMBP.attlocal.net>
2022-10-27 17:05:43 -0400

    FirstCommit adding project

---
+++ JenkinsDemo/JenkinsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsDemoApp.swift
+//  JenkinsDemo
+//
+//  Created by Philip Al-Twal on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
